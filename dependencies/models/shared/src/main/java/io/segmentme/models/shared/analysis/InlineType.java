21df34166c69af5b83c4daf5c16b27f6af01048b
Artem Slednev <artemslednev@coherentsolutions.com>
2021-04-09 16:10:11 +0300

    removed: InlineType fix

--- dependencies/models/shared/src/main/java/io/segmentme/models/shared/analysis/InlineType.java
+++ dependencies/models/shared/src/main/java/io/segmentme/models/shared/analysis/InlineType.java
@@ -2,9 +2,11 @@ package io.segmentme.models.shared.analysis;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class InlineType {
     private SchemaNodeType rootType;
     private SchemaNodeType subType;
