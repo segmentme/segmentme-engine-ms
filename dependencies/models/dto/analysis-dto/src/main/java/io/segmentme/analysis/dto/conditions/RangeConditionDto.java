ae2210f73456c64a20a9913641cd56a36ce548f9
Volad <vak.kondratenko@gmail.com>
2021-07-29 16:56:41 +0300

    decoupeling services. tests broken

--- dependencies/models/dto/analysis-dto/src/main/java/io/segmentme/analysis/dto/conditions/RangeConditionDto.java
+++ dependencies/models/dto/analysis-dto/src/main/java/io/segmentme/analysis/dto/conditions/RangeConditionDto.java
@@ -1,7 +1,14 @@
 package io.segmentme.analysis.dto.conditions;
 
-import io.segmentme.models.shared.analysis.RangeValue;
 
-public class RangeConditionDto extends SimpleConditionDto<RangeValue> {
+import lombok.Data;
 
+public class RangeConditionDto extends SimpleConditionDto<RangeConditionDto.RangeValue> {
+
+    @Data
+    public static class RangeValue {
+        private Object min;
+
+        private Object max;
+    }
 }
