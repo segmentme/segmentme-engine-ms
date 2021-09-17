fc5b2970f96d1636927cca780ee6ef57a45205d3
Volad <vak.kondratenko@gmail.com>
2021-09-17 14:31:29 +0300

    work on kubernetes deployment

--- kubernetes/infrastructure/kube-create-auth0-secrets.sh
+++ kubernetes/infrastructure/kube-create-auth0-secrets.sh
@@ -3,6 +3,6 @@
 #https://kubernetes.io/docs/concepts/containers/images/#configuring-nodes-to-authenticate-to-a-private-registry
 export AUTH0_CLIENT_ID_ENC=`echo $AUTH0_CLIENT_ID|base64`
 export AUTH0_CLIENT_SECRET_ENC=`echo $AUTH0_CLIENT_SECRET|base64`
-
+kubectl delete secrets auth0-secret 2> /dev/null
 envsubst < ./auth-secret.yml | kubectl apply -f -
 echo "Auth0 secret created"
