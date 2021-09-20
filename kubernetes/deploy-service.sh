8fdc4f97ec7edaf34eeefc9ba2a19e850a65a276
Volad <vak.kondratenko@gmail.com>
2021-09-20 16:22:17 +0300

    work on kubernetes deployment

--- kubernetes/deploy-service.sh
+++ kubernetes/deploy-service.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 appname=$1
+IMAGE_NAME="$DOCKER_REGISTRY/segmentme-$appname:$CIRCLE_SHA1"
+export IMAGE_NAME
 
 waitForDeploymentStart(){
     while [[ $(kubectl get pods -l app=$1 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod $1" && sleep 1; done
